413ec125ff522215ef4411025c73771c9fea5ee0
kthth0707 <kimtaehyun00728@gmail.com>
2022-09-22 23:12:54 +0900

    Initial Commit

---
+++ COVID-19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID-19
+//
+//  Created by TH on 2022/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
