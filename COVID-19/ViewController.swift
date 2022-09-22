8178bfbd0dc33bff4617e8b06f5cf7b112481641
kthth0707 <kimtaehyun00728@gmail.com>
2022-09-22 23:38:44 +0900

    코로나 19 완성~~~

--- COVID-19/ViewController.swift
+++ COVID-19/ViewController.swift
@@ -1,19 +1,137 @@
 //
 //  ViewController.swift
-//  COVID-19
+//  COVID19
 //
-//  Created by TH on 2022/09/22.
+//  Created by Gunter on 2021/09/18.
 //
 
 import UIKit
 
+import Alamofire
+import Charts
+
 class ViewController: UIViewController {
+  @IBOutlet weak var totalCaseLabel: UILabel!
+  @IBOutlet weak var newCaseLabel: UILabel!
+  @IBOutlet weak var pieChartView: PieChartView!
+  @IBOutlet weak var labelStackView: UIStackView!
+  @IBOutlet weak var indicatorView: UIActivityIndicatorView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.indicatorView.startAnimating()
+    self.fetchCovidOverview(completionHandler: { [weak self] result in
+      guard let self = self else { return }
+      self.indicatorView.stopAnimating()
+      self.indicatorView.isHidden = true
+      self.labelStackView.isHidden = false
+      self.pieChartView.isHidden = false
+      switch result {
+      case let .success(result):
+        self.configureStackView(koreaCovidOverview: result.korea)
+        let covidOverviewList = self.makeCovidOverviewList(cityCovidOverview: result)
+        self.configureChartView(covidOverviewList: covidOverviewList)
+
+      case let .failure(error):
+        debugPrint("failure \(error)")
+      }
+    })
+  }
+
+  func makeCovidOverviewList(cityCovidOverview: CityCovidOverview) -> [CovidOverview] {
+    return [
+      cityCovidOverview.seoul,
+      cityCovidOverview.busan,
+      cityCovidOverview.daegu,
+      cityCovidOverview.incheon,
+      cityCovidOverview.gwangju,
+      cityCovidOverview.daejeon,
+      cityCovidOverview.ulsan,
+      cityCovidOverview.sejong,
+      cityCovidOverview.gyeonggi,
+      cityCovidOverview.chungbuk,
+      cityCovidOverview.chungnam,
+      cityCovidOverview.jeonnam,
+      cityCovidOverview.gyeongbuk,
+      cityCovidOverview.gyeongnam,
+      cityCovidOverview.jeju,
+    ]
+  }
+
+  func configureStackView(koreaCovidOverview: CovidOverview) {
+    self.totalCaseLabel.text = "\(koreaCovidOverview.totalCase) 명"
+    self.newCaseLabel.text = "\(koreaCovidOverview.newCase) 명"
+  }
+
+  func configureChartView(covidOverviewList: [CovidOverview]) {
+    self.pieChartView.delegate = self
+    let entries = covidOverviewList.compactMap { [weak self] overview -> PieChartDataEntry? in
+      guard let self = self else { return nil }
+      return PieChartDataEntry(
+        value: self.removeFormatString(string: overview.newCase),
+        label: overview.countryName,
+        data: overview
+      )
     }
+    let dataSet = PieChartDataSet(entries: entries, label: "코로나 발생 현황")
+    dataSet.sliceSpace = 1
+    dataSet.entryLabelColor = .black
+    dataSet.xValuePosition = .outsideSlice
+    dataSet.valueTextColor = .black
+    dataSet.valueLinePart1OffsetPercentage = 0.8
+    dataSet.valueLinePart1Length = 0.2
+    dataSet.valueLinePart2Length = 0.3
 
+    dataSet.colors = ChartColorTemplates.vordiplom()
+      + ChartColorTemplates.joyful()
+      + ChartColorTemplates.colorful()
+      + ChartColorTemplates.liberty()
+      + ChartColorTemplates.pastel()
+      + ChartColorTemplates.material()
 
+    self.pieChartView.data = PieChartData(dataSet: dataSet)
+    self.pieChartView.spin(duration: 0.3, fromAngle: pieChartView.rotationAngle, toAngle: pieChartView.rotationAngle + 80)
+  }
+
+  func removeFormatString(string: String) -> Double {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .decimal
+    return formatter.number(from: string)?.doubleValue ?? 0
+  }
+
+  func fetchCovidOverview(
+    completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+  ) {
+    let url = "https://api.corona-19.kr/korea/country/new/"
+    let param = [
+      "serviceKey": "5QsoaWx2E9kvfeYOrbuiI3wTqL8JgFcGC"
+    ]
+    AF.request(url, method: .get, parameters: param)
+      .responseData(completionHandler: { response in
+        switch response.result {
+        case let .success(data):
+          do {
+            let decoder = JSONDecoder()
+            let result = try decoder.decode(CityCovidOverview.self, from: data)
+            completionHandler(.success(result))
+          } catch {
+            completionHandler(.failure(error))
+          }
+        case let .failure(error):
+          completionHandler(.failure(error))
+        }
+      })
+  }
+}
+
+extension ViewController: ChartViewDelegate {
+  func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+    guard let covidDetailViewController = self.storyboard?.instantiateViewController(identifier: "CovidDetailViewController") as? CovidDetailViewController else {
+         return
+       }
+    guard let covidOverview = entry.data as? CovidOverview else { return }
+    covidDetailViewController.covidOverview = covidOverview
+    self.navigationController?.pushViewController(covidDetailViewController, animated: true)
+  }
 }
 
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'COVID-19' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Alamofire'
+  pod 'Charts'
+
+
+  # Pods for COVID-19
+
+  target 'COVID-19Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'COVID-19UITests' do
+    # Pods for testing
+  end
+
+end
